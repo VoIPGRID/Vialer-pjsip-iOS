3664b63959f83944a86bff5638c92b152ce394fc
Smolski Aleksey <smolsky.aleksey@me.com>
2017-01-06 18:24:31 +0300

    enable bg socket

--- config_site.h
+++ config_site.h
@@ -4,6 +4,4 @@
 #define PJ_CONFIG_IPHONE 1
 #define PJMEDIA_HAS_OPUS_CODEC 1
 #define PJMEDIA_HAS_VIDEO 1
-#define PJ_IPHONE_OS_HAS_MULTITASKING_SUPPORT 0
-#define PJ_ACTIVESOCK_TCP_IPHONE_OS_BG 0
 #include <pj/config_site_sample.h>
