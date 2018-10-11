ceeb4751e95a57171cd4263420164e7b7384c664
Redmer Loen <redmer.loen@wearespindle.com>
2018-10-11 14:24:31 +0200

    Fix for x86_64

--- VialerPJSIP.framework/Versions/A/Headers/openssl/opensslconf_ios_armv7.h
+++ VialerPJSIP.framework/Versions/A/Headers/openssl/opensslconf_ios_armv7.h
@@ -119,8 +119,8 @@ extern "C" {
 
 #if !(defined(VMS) || defined(__VMS)) /* VMS uses logical names instead */
 #if defined(HEADER_CRYPTLIB_H) && !defined(OPENSSLDIR)
-#define ENGINESDIR "/Users/redmerloen/Programming/Vialer/iOS/Vialer-pjsip-iOS/pjsip/external/ssl/bin/iPhoneOS11.4-armv7.sdk/lib/engines"
-#define OPENSSLDIR "/Users/redmerloen/Programming/Vialer/iOS/Vialer-pjsip-iOS/pjsip/external/ssl/bin/iPhoneOS11.4-armv7.sdk"
+#define ENGINESDIR "/Users/redmerloen/Programming/Vialer/iOS/Vialer-pjsip-iOS/pjsip/external/ssl/bin/iPhoneOS12.0-armv7.sdk/lib/engines"
+#define OPENSSLDIR "/Users/redmerloen/Programming/Vialer/iOS/Vialer-pjsip-iOS/pjsip/external/ssl/bin/iPhoneOS12.0-armv7.sdk"
 #endif
 #endif
 
